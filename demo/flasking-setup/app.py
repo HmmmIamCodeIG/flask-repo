497b831044584932908986ef0d2a6558cbb497e8
ueueueueue <jwu.2026@trinity.nsw.edu.au>
2025-10-17 12:40:25 +1100

    YAYY!!!!!!

--- demo/flasking-setup/app.py
+++ demo/flasking-setup/app.py
@@ -4,13 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # return Index 
     return render_template('index.html')
 
-'''
+@app.route('/')
+def dashboard():
+    return render_template('dashboard.html')
+
 @app.route('/login')
 def login():
     return render_template('login.html')
-'''
 
 if __name__ == '__main__':
     app.run(debug=True)
