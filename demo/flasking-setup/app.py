5618717df74b2efb3555ff59d774e81c98df18db
ueueueueue <jwu.2026@trinity.nsw.edu.au>
2025-11-03 15:59:39 +1100

    new computer!!!!

--- demo/flasking-setup/app.py
+++ demo/flasking-setup/app.py
@@ -1,42 +1,12 @@
-from flask import Flask, render_template, request
-from dbm import sqlite3
+from flask import Flask
 
 app = Flask(__name__)
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-
-    # insecure: plain text password comparison
-    conn = get_db_connection()
-    cursor = conn.cursor()
-
-    # using parameterized queries to prevent SQL injection
-    return conn
 
 
 @app.route('/')
-def index():
-    # return Index 
-    return render_template('index.html')
+def hello_world():  # put application's code here
+    return 'Hello World!'
 
-@app.route('/')
-def dashboard():
-    return render_template('dashboard.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-@app.route('/register', methods=["POST"])
-def register():
-    if request.method == "POST":
-        username = request.form['username']
-        display_name = request.form['display_name']
-        email = request.form['email']
-        password = request.form['password']
-        confirm_password = request.form['confirm_password']
-
-    return render_template('register.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
